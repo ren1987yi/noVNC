5bd3c214a72c7cf8da0ec3c2bb8a3ee282883309
毅 任 <ren1987yi@163.com>
2025-10-21 10:23:58 +0800

    update

--- run.bat
+++ run.bat
@@ -8,7 +8,9 @@ cd /d "%~dp0"
 REM 如需 HTTPS，可增加 --cert/--key
 REM 监听 6080，把 token 交给 ParamTokenPlugin 解析
 
-start "" "http://127.0.0.1:6080/main.html"
+REM 启动直接进主页
+start "" "http://127.0.0.1:6080"
+
 set PARAM_TOKEN_PLUGIN_LOG=DEBUG
 python -m websockify --web . 6080 --token-plugin "param_token_plugin.ParamTokenPlugin"
 
